3cdab94c12d6c12382530ba8c932425fdfdc2b43
horbel <gorbel.alex@ya.ru>
2016-01-30 16:33:26 +0200

    css

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -5,8 +5,10 @@ using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using System.Collections.Generic;
 using Moq;
-//using System.Web.Mvc;
+using SportsStore.WebUI.HtmlHelpers;
+using System.Web.Mvc;
 using System.Linq;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -29,11 +31,86 @@ namespace SportsStore.UnitTests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             //act
-            IEnumerable<Product> products = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel model = (ProductsListViewModel)controller.List(null,2).Model;
             //assert
+            IEnumerable<Product> products = model.Products;
             Assert.IsTrue(products.ToArray().Length == 2);
             Assert.AreEqual(products.ToArray()[0].Name, "P4");
             Assert.AreEqual(products.ToArray()[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - define an HTML helper - we need to do this
+            // in order to apply the extension method
+            HtmlHelper myHelper = null;
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+            //act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+            //assert
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a>"
+                + @"<a class=""selected"" href=""Page2"">2</a>"
+                + @"<a href=""Page3"">3</a>");
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                 {
+                new Product {ProductID = 1, Name = "P1" },
+                new Product {ProductID = 2, Name = "P2" },
+                new Product {ProductID = 3, Name = "P3" },
+                new Product {ProductID = 4, Name = "P4" },
+                new Product {ProductID = 5, Name = "P5" },
+                 }.AsQueryable);
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            //act
+            ProductsListViewModel model = (ProductsListViewModel)controller.List(null,2).Model;
+            //assert
+            Assert.AreEqual(model.Products.Count(), 2);
+            Assert.AreEqual(model.pagingInfo.CurrentPage, 2);
+            Assert.AreEqual(model.pagingInfo.TotalPages, 2);
+            Assert.AreEqual(model.pagingInfo.ItemsPerPage, 3);
+            Assert.AreEqual(model.pagingInfo.TotalItems, 5);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "1", Category = "Cat1" },
+                new Product {ProductID = 2, Name = "2", Category = "Cat2" },
+                new Product {ProductID = 3, Name = "3", Category = "Cat1" },
+                new Product {ProductID = 4, Name = "4", Category = "Cat2" },
+                new Product {ProductID = 5, Name = "5", Category = "Cat3" },
+            }.AsQueryable);
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            //act
+            ProductsListViewModel model = (ProductsListViewModel)controller.List("Cat1", 1).Model;
+            Product[] products = model.Products.ToArray();
+            //assert
+            Assert.AreEqual(products.Length, 2);
+            Assert.IsTrue(products[0].Name == "1" && products[0].Category == "Cat1");
+            Assert.IsTrue(products[1].Name == "3" && products[1].Category == "Cat1");
+
+
         }
     }
 }
--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,23 +5,38 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
-    {
+    {        
         public int PageSize = 4;
         private IProductRepository repository;
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products
+            ProductsListViewModel model = new ProductsListViewModel();
+            model.Products = repository.Products
+                .Where(p => category==null || p.Category == category)
                 .OrderBy(p => p.ProductID)
                 .Skip((page - 1) * PageSize)
-                .Take(PageSize));                
+                .Take(PageSize);
+            model.pagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            };
+            model.CurrentCategory = category;
+            
+                             
+            return View(model);
+                            
         }
     }
 }
